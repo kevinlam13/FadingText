ae65084e1365410a1ff25964010c19cf3b247b9b
Kevin Lam <132787167+kevinlam13@users.noreply.github.com>
2025-09-30 22:26:51 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,36 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(const AppRoot());
+
+class AppRoot extends StatefulWidget {
+  const AppRoot({super.key});
+  @override
+  State<AppRoot> createState() => _AppRootState();
 }
 
-class MyApp extends StatelessWidget {
+class _AppRootState extends State<AppRoot> {
+  ThemeMode _mode = ThemeMode.light;
+  void _toggleTheme() =>
+      setState(() => _mode = _mode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: FadingTextAnimation(),
+      title: 'Fading Text',
+      debugShowCheckedModeBanner: false,
+      themeMode: _mode,
+      theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.indigo, brightness: Brightness.light),
+      darkTheme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.indigo, brightness: Brightness.dark),
+      home: FadingOpacityPage(onToggleTheme: _toggleTheme, isDark: _mode == ThemeMode.dark),
     );
   }
 }
 
-class FadingTextAnimation extends StatefulWidget {
+class FadingOpacityPage extends StatefulWidget {
+  final VoidCallback onToggleTheme;
+  final bool isDark;
+  const FadingOpacityPage({super.key, required this.onToggleTheme, required this.isDark});
+
   @override
   State<FadingOpacityPage> createState() => _FadingOpacityPageState();
 }
@@ -26,7 +43,18 @@ class _FadingOpacityPageState extends State<FadingOpacityPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Fading Text Animation'),
+        title: const Text('Fading (AnimatedOpacity)'),
+        actions: [
+          IconButton(
+            tooltip: 'Toggle theme',
+            onPressed: widget.onToggleTheme,
+            icon: Icon(widget.isDark ? Icons.light_mode : Icons.dark_mode),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _toggle,
+        child: const Icon(Icons.play_arrow),
       ),
       body: Center(
         child: AnimatedOpacity(
